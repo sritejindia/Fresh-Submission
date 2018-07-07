95f71a007793baca757b9007c4258e85c3a49c6d
Sritej Kurapati <sritejkurapati@Sritejs-MacBook-Air.local>
2018-07-07 18:07:58 +0530

    Version 2.0

--- SeleniumFactory/src/test/java/com/katalon/demoaut/tests/FirstTest.java
+++ SeleniumFactory/src/test/java/com/katalon/demoaut/tests/FirstTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FirstTest {
@@ -43,6 +44,24 @@ public class FirstTest {
 
         webDriverWait = new WebDriverWait(driver,10);
         webDriverWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("appointment")));
+
+        Select facilityDropdown = new Select(driver.findElement(By.id("combo_facility")));
+        facilityDropdown.selectByVisibleText("Hongkong CURA Healthcare Center");
+        driver.findElement(By.id("chk_hospotal_readmission")).click();
+        driver.findElement(By.cssSelector("input[type='radio'][name='programs'][value='Medicaid']"));
+        driver.findElement(By.id("txt_visit_date")).sendKeys("12/09/2018");
+        driver.findElement(By.id("txt_comment")).sendKeys("Thus made an appointment");
+        driver.findElement(By.id("btn-book-appointment")).click();
+
+        webDriverWait = new WebDriverWait(driver,10);
+        webDriverWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("summary")));
+
+        System.out.println("The facility chosen is: " + driver.findElement(By.id("facility")).getText());
+        System.out.println("The hospital readmission is: " + driver.findElement(By.id("hospital_readmission")).getText());
+        System.out.println("The program chosen is: " + driver.findElement(By.id("program")).getText());
+        System.out.println("Your comments are: " + driver.findElement(By.id("comment")).getText());
+        driver.findElement(By.id("menu-toggle")).click();
+        driver.findElement(By.linkText("Logout")).click();
 
     }
 
